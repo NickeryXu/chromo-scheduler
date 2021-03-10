9430f302e975617f9d8f5b994a0f024c36df8c30
Xie Di <xiedidan@yeah.net>
2021-03-10 15:22:01 +0800

    change init table

--- create_sql_tables.py
+++ create_sql_tables.py
@@ -17,6 +17,21 @@ def create_sql_tables(db_name):
     finally:
         if conn:
             conn.close()
+def create_sql_web_tables(db_name):
+    try:
+        conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES)
+        cursor = conn.cursor()
 
+        cursor.execute('create table users(id integer primary key autoincrement, name varchar(50), hash_password varchar(100), salt varchar(100), is_admin bool)')
+        cursor.execute('create table setting(id integer primary key autoincrement, debug bool, src_path varchar(200), export_path varchar(200), dest_path varchar(200), tmp_path varchar(200), src_ext varchar(200), export_ext varchar(200), sort_simulation bool, worker_check_interval int)')
+
+        conn.commit()
+    except sqlite3.Error as error:
+        print('sqlite3 error, {}'.format(error))
+        return False
+    finally:
+        if conn:
+            conn.close()
 if __name__ == '__main__':
     create_sql_tables(db_name)
+    create_sql_web_tables(db_name)
